2c39507f8d6c6060879f531dca83c0b94c296196
zm <602187256@qq.com>
2020-03-20 16:21:33 +0800

    增加  开启简易模式

--- src/components/view.vue
+++ src/components/view.vue
@@ -5,18 +5,25 @@
     export default {
         props:['wpsUrl','token'],
         data(){
-            return{}
+            return{
+                // 是否开启简易模式
+                simpleMode:false
+            }
         },
         mounted(){
             this.openWps(this.wpsUrl,this.token);
         },
         methods:{
-            openWps(url,token){
-                let wps = this.wps.config({
+            openWps(url, token) {
+                let _this = this;
+                const wps = _this.wps.config({
+                    mode: _this.simpleMode?'simple':'normal',
                     mount: document.querySelector('#app'),
-                    wpsUrl: url
+                    wpsUrl: url,
                 });
-                wps.setToken({"token":token});
+                wps.setToken({"token": token});
+                let app = wps.Application;
+                this.console.log(JSON.stringify(app))
             }
         }
     }
